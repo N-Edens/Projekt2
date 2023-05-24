1d8856529e07124057305a0f6a67a92162a2b016
N-Edens <128368138+N-Edens@users.noreply.github.com>
2023-05-24 10:19:16 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,42 +1,42 @@
-// server.js
-
-
 const express = require('express');
-const app = express();
 const { Client } = require('pg');
+const cors = require('cors');
+const path = require('path');
+//const url = "https://dts-gfjg.onrender.com"
+const { url } = require('os');
 
+const app = express();
+const port = process.env.PORT || 3000;
+const hostname = 'dts.onrender.com';
 
-// Opret forbindelse til din Elephant SQL database
-const client = new Client({
- connectionString: 'postgres://pwyctilp:17nsLdJ1cvO6X5reg0FfcQFNQge5ARfX@balarama.db.elephantsql.com/pwyctilp',
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'projekt')));
+app.get('/favicon.ico', (req, res) => {
+  res.status(204);
 });
 
+const client = new Client({
+  connectionString: 'postgres://pwyctilp:17nsLdJ1cvO6X5reg0FfcQFNQge5ARfX@balarama.db.elephantsql.com/pwyctilp',
+});
 
-// Opret forbindelse til databasen
 client.connect();
 
-
-// Definer en rute for at hente data fra databasen
 app.get('/vej', (req, res) => {
- // Udfør en forespørgsel til databasen
- client.query('SELECT * FROM vej', (err, result) => {
-   if (err) {
-     console.error('Fejl ved hentning af data fra databasen', err);
-     res.status(500).send('Der opstod en fejl');
-   } else {
-     // Send data som JSON-respons
-     res.json(result.rows);
-   }
- });
+  client.query('SELECT vejnavn, AVG(gennemsnit) AS gennemsnit FROM vej GROUP BY vejnavn ORDER BY gennemsnit DESC LIMIT 4;', (err, result) => {
+    if (err) {
+      console.error('Fejl ved hentning af data fra databasen', err);
+      res.status(500).send('Der opstod en fejl');
+    } else {
+      res.json(result.rows);
+    }
+  });
 });
 
-
-// Start serveren
-app.listen(3000, () => {
- console.log('Serveren kører på port 3000');
+app.listen(port, () => {
+  console.log('Serveren kører på 3000');
 });
 
-
-process.on('exit',() => {
-client.end();
+process.on('exit', () => {
+  client.end();
 });
